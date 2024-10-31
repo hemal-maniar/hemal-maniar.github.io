82b10cf5716be45d6d238cbd0da8615f185ded8a
Hemal Maniar <hmaniar07@gmail.com>
2024-10-31 12:31:41 +0530

    JS change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 // Gets the current year for footer
 var date = new Date();
 var currentYear = date.getFullYear();
-document.getElementById("date").innerHTML = "2019 -  " + currentYear + " | Benjamin J. Carlson";
+document.getElementById("date").innerHTML = "2024 -  " + currentYear + " | Hemal Maniar";
 
 // Sticky navigation bar
 window.onscroll = function () { myFunction() };
