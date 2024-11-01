e36f6a61f572e190e3634ac886f5c14372122105
Hemal Maniar <hmaniar07@gmail.com>
2024-11-01 11:18:08 +0530

    minor changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@ function myFunction() {
 }
 
 // Subheading terminal effect
-consoleText(['Security Engineer', 'Cybersecurity Analyst', 'Penetration Tester', 'Blogger', 'OSCP Certified'], 'text');
+consoleText(['a Security Engineer', 'a Penetration Tester', 'a Cybersecurity Analyst', 'a Blogger', 'OSCP Certified'], 'text');
 
 function consoleText(words, id, colors) {
     if (colors === undefined) colors = ['#2e3141'];
